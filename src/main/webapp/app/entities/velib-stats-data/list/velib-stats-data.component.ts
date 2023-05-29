c69eeeafe5ac01b3904cd250d2d969551cf6a433
attia <anthony.attia1@gmail.com>
2023-05-29 07:41:59 +0200

    feat(front): ajout top 3 station

--- src/main/webapp/app/entities/velib-stats-data/list/velib-stats-data.component.ts
+++ src/main/webapp/app/entities/velib-stats-data/list/velib-stats-data.component.ts
@@ -12,11 +12,13 @@ export class VelibStatsDataComponent implements OnInit {
   public velibDataApiResponse: VelibStatsDataApiResponse | null = new VelibStatsDataApiResponse();
 
   constructor(protected velibStatsDataService: VelibStatsDataService) {
+
   }
 
   ngOnInit(): void {
     this.velibStatsDataService.getVelibStatData().subscribe((res: HttpResponse<VelibStatsDataApiResponse>) => {
       this.velibDataApiResponse = res.body
+      this.getTopThreeStation(this.velibDataApiResponse?.stationNameHourAverageBikeAvailable);
     })
   }
 
@@ -45,6 +47,34 @@ export class VelibStatsDataComponent implements OnInit {
   sum(hour: string): number {
     return Number(hour) + 1;
   }
+
+  getTopThreeStation(data: StationData | undefined): any[] {
+    let stationTopThreeMap = new Map<string, number>;
+    if (data) {
+      for (const station of this.getStations(data)) {
+        let sumBikeAvaibableTotal = 0
+        for (const hour of this.getHours(data)) {
+          sumBikeAvaibableTotal = sumBikeAvaibableTotal + Math.round(this.getAvailability(data[station], hour));
+        }
+        stationTopThreeMap.set(station, Math.round(sumBikeAvaibableTotal / this.getHours(data).length));
+
+      }
+    }
+    return this.sortAndSliceMap(stationTopThreeMap, 3);
+  }
+
+  /**
+   * Return, un Array trié par valeur croissante
+   * @param map, map qu'on souhaite trié et transformé en Array
+   * @param count, nombre d'élément à retirer de l'Array.
+   */
+  sortAndSliceMap(map: Map<string, number>, count: number): any[] {
+    const sortedArray = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
+    const slicedArray = sortedArray.slice(0, count);
+    console.log(slicedArray);
+    return slicedArray;
+  }
+
 }
 
 
